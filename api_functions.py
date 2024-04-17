daf2482d42a921b9e34dc6915f1f83ac98761346
marianela-kesme <132613464+marianela-kesme@users.noreply.github.com>
2024-04-17 09:22:53 +0300

    primer commit

---
+++ api_functions.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import pyarrow as pq
+import json
+import os
+from fastapi import FastAPI
+
+
+# Importaciones
+from fastapi import FastAPI, Query
+from fastapi.responses import HTMLResponse
+import api_functions as af
+
+import importlib
+importlib.reload(af)
+
+# Se instancia la aplicación
+app = FastAPI()
+
+# Funciones
+@app.get(path="/", 
+         response_class=HTMLResponse,
+         tags=["Home"])
+def home():
+    '''
+    Página de inicio que muestra una presentación.
+
+    Returns:
+    HTMLResponse: Respuesta HTML que muestra la presentación.
+    '''
+    return af.presentacion()
+
+@app.get(path = '/userdata',
+          description = """ <font color="blue">
+                        INSTRUCCIONES<br>
+                        1. Haga clik en "Try it out".<br>
+                        2. Ingrese el user_id en el box abajo.<br>
+                        3. Scrollear a "Resposes" para ver la cantidad de dinero gastado por el usuario, el porcentaje de recomendación que realiza el usuario y cantidad de items que tiene el mismo.
+                        </font>
+                        """,
+         tags=["Consultas Generales"])
+def userdata(user_id: str = Query(..., 
+                                description="Identificador único del usuario", 
+                                example="EchoXSilence")):
+        
+    return af.userdata(user_id)
+
+
+@app.get(path = '/developer',
+          description = """ <font color="blue">
+                        1. Haga clik en "Try it out".<br>
+                        2. Ingrese el nombre del desarrollador en el box abajo.<br>
+                        3. Scrollear a "Resposes" para ver la cantidad de items y porcentaje de contenido Free por año de ese desarrollador.
+                        </font>
+                        """,
+         tags=["Consultas Generales"])
+def developer(desarrollador: str = Query(..., 
+                            description="Desarrollador del videojuego", 
+                            example='Valve')):
+    return af.developer(desarrollador)
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+    
+
+
+    
+
+
